72bad07fabe7d601a5d4b136f6ed1c7032b1ada2
AndrewBrodsky <andrew@brodskyresearch.com>
2017-11-28 13:19:29 -0700

    Update P-Value-Extractor.py

--- P-Value-Extractor.py
+++ P-Value-Extractor.py
@@ -1,5 +1,32 @@
 
-# coding: utf-8
+# This code extracts p-values and other relevant methodology and results from journal articles. 
+# Andrew adapted it from a similar file by Robert Lanfear in order to extract other useful infomrmation,
+#such as methodology, from journal articles. Ultimately the goal is to create a training database
+#for machine learning.
+
+# This Python file uses the following encoding: utf-8
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Robert Lanfear
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.# It was adapated from coding: utf-8
 
 # In[1]:
 
